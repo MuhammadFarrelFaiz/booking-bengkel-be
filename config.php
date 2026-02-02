0b7a21138f5fb3254acc492e52b89430fd8553c7
MuhammadFarrelFaiz <farrelfaizrabbani@gmail.com>
2026-02-02 09:19:21 +0700

    Remove `UPLOAD_PATH` definition from config and `/uploads/` from .gitignore.

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_PASS', '123');
 define('DB_NAME', 'db_bengkel_booking');
 
 define('BASE_URL', 'http://localhost:8000/');
-define('UPLOAD_PATH', 'uploads/mobil/');
+
 
 define('JAM_BUKA', '08:00');
 define('JAM_TUTUP', '17:00');
